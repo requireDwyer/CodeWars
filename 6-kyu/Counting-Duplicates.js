7ef1a25d304664c02743754f23f34fadcdaac35d
Aidan Dwyer <requiredwyer@gmail.com>
2021-06-08 23:53:01 -0400

    add counting duplicates 6kyu

---
+++ 6-kyu/Counting-Duplicates.js
@@ -0,0 +1,17 @@
+function duplicateCount(text) {
+  const charCount = {};
+  for (let char of text) {
+    char = char.toLowerCase();
+    if (char in charCount) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  let sumOfDuplicate = 0;
+  for (const char in charCount) {
+    if (charCount[char] > 1) sumOfDuplicate += 1;
+  }
+  console.log(sumOfDuplicate);
+  return sumOfDuplicate;
+}
